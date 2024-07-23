3400bac2145ea0eb2886128431bf75bb5e957728
Hamdan Faiz <faizpostbox@gmail.com>
2024-07-23 10:33:58 +0700

    error integration css

---
+++ nuxt.config.js
@@ -0,0 +1,86 @@
+export default {
+
+  // Target Deployment
+  target: 'server',
+
+  //rendering mode SSR	
+  ssr: true,
+
+  loading: {
+    color: 'white',	// <-- color
+    height: '5px'	// <-- height
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-ecommerce',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: ''
+      }
+    ],
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/images/logo.png'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
+      },
+    ],
+    script: [
+      { src: '/js/coreui.bundle.min.js' }, 
+      { src: 'https://app.sandbox.midtrans.com/snap/snap.js', 'data-client-key':'SB-Mid-client-W-hu7En5B48GLeWp' }, 
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/css/style.min.css',
+    '@/assets/css/custom.css',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    baseURL: 'http://localhost:3000'
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
